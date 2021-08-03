22a83c61c4406beb2da6ef2fd0873c02336bfbc5
kirk-zZ <79148505+kirk-zZ@users.noreply.github.com>
2021-08-03 22:49:10 +0800

    back to welcome
    wala lang

--- src/app/CRM/main/main.component.spec.ts
+++ src/app/CRM/main/main.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MainComponent } from './main.component';
+import { CRMmainComponent } from './main.component';
 
-describe('MainComponent', () => {
-  let component: MainComponent;
-  let fixture: ComponentFixture<MainComponent>;
+describe('CRMmainComponent', () => {
+  let component: CRMmainComponent;
+  let fixture: ComponentFixture<CRMmainComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MainComponent ]
+      declarations: [ CRMmainComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MainComponent);
+    fixture = TestBed.createComponent(CRMmainComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
